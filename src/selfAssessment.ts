5e925e84c88b91b3cc3ad5e3ef3df5b05221b012
Daniel Ramirez <danielramirezhinojosa@gmail.com>
2017-01-19 17:12:47 -0600

    Poner rango a grafica y colocar lineas para mostrar limite inferior y superios

--- src/selfAssessment.ts
+++ src/selfAssessment.ts
@@ -106,11 +106,13 @@ export class SelfAssessment {
         value: dimension.points / dimension.maxPoints
       });
     }
+
     nv.addGraph(() => {
       let chart = nv.models.discreteBarChart()
         .x((d) => { return d.label; })
         .y((d) => { return d.value; })
         .color((d) => { return this.processColor(d); })
+        .forceY([0,1])
         .margin({bottom: 200});
 
       chart.xAxis.rotateLabels(-90);
@@ -126,6 +128,26 @@ export class SelfAssessment {
 
       nv.utils.windowResize(chart.update);
 
+      var g = d3.select('#dimensionsChart svg');
+
+      var yValueScale = chart.yAxis.scale();
+
+      g.append("line")
+      .style("stroke", "#3FAE49")
+      .style("stroke-width", "2.5px")
+      .attr("x1", 70 )
+      .attr("y1", yValueScale(.84))
+      .attr("x2", 700)
+      .attr("y2", yValueScale(.84));
+
+      g.append("line")
+      .style("stroke", "#BDA831")
+      .style("stroke-width", "2.5px")
+      .attr("x1", 70 )
+      .attr("y1", yValueScale(.69))
+      .attr("x2", 700)
+      .attr("y2", yValueScale(.69));
+
       return chart;
     });
   }
@@ -138,7 +160,7 @@ export class SelfAssessment {
     let data = {
       "title":"Resultado",
       "subtitle":"EDUCA",
-      "ranges":[55,70,85,100], 
+      "ranges":[55,70,85,100],
       "rangeLabels":['Alto riesgo','Inestable','Estable','En consolidación'],
       "measures":[score],
       "markers":[70, averageScore],
@@ -151,7 +173,7 @@ export class SelfAssessment {
         .datum(data)
         .transition().duration(1000)
         .call(chart);
-        
+
       nv.utils.windowResize(chart.update);
 
       return chart;
@@ -248,4 +270,3 @@ export class SelfAssessment {
   }
 
 }
-
