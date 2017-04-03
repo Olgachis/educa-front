bc0f16b72b91438654db0e7588096880c8d4d81c
Daniel Ramirez <danielramirezhinojosa@gmail.com>
2017-04-02 21:27:14 -0500

    Vista para el tablero

--- src/dashboard.ts
+++ src/dashboard.ts
@@ -16,11 +16,65 @@ export class Dashboard {
     this.config = config;
   }
 
+  processColor(d) {
+    let score = d.points / d.maxPoints;
+    if(score < 0.55) {
+      return '#C3002F';
+    } else if(score < 0.7) {
+      return '#BDA831';
+    } else if(score < 0.85) {
+      return '#3FAE49';
+    } else {
+      return '#365E9E';
+    }
+  }
+
+  processLabel(d) {
+    let score = d.points / d.maxPoints;
+    if(score < 0.55) {
+      return 'Alto riesgo';
+    } else if(score < 0.7) {
+      return 'Inestable';
+    } else if(score < 0.85) {
+      return 'Estable';
+    } else {
+      return 'En consolidación';
+    }
+  }
+
+  transformCoso(el ){
+
+    return {
+      id: el['id'].number,
+      name: el['id'].name,
+      maxPoints: el['maxPoints'],
+      maxQuestions: el['maxPoints'],
+      minimumRequiredQuestions: el['maxPoints'],
+      points: el['points'],
+      score: (el['points'] / el['maxPoints'] * 100).toFixed(1)
+    };
+  }
+
   async activate(): Promise<void> {
     await fetch;
     this.api = new Api(this.config);
     Utils.showSpinner();
     const summary = await this.api.fetch('/api/summary/listPrimaryCampuses');
+    this.summary = (await summary.json()).map(r => {
+
+      r = r.questionnaireResults;
+      r.institutionName = r.institutionName;
+      let dimensions = _.map(r.dimensionResults, this.transformCoso);
+
+      r.canComplete = r.questions == r.maxQuestions;
+      r.completed = (r.questions == r.maxQuestions) && !r.openQuestionnaire;
+      r.score = (r.points / r.maxPoints * 100).toFixed(3);
+      r.level = this.processLabel(r);
+      r.missingQuestions = r.maxQuestions - r.questions;
+      r.color = this.processColor(r);
+      r.dimensions = dimensions;
+      return r;
+    });
     Utils.hideSpinner();
   }
 }
